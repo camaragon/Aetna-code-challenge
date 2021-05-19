a5229bf776cbbedc8672c74f1bdbdfed5e90a39f
Cameron Aragon <caragon4695@gmail.com>
2021-05-18 20:25:24 -0600

    Add initial sample request to API

--- test/apiTest.spec.js
+++ test/apiTest.spec.js
@@ -4,6 +4,20 @@ const chai = require('chai');
 const expect = chai.expect;
 const baseUrl = 'http://www.omdbapi.com/';
 
-// describe('Initial OMDb API test', () => {
-//     it('Should return a response ')
-// })
+describe('Initial OMDb API test', () => {
+    it('Should return a response ', (done) => {
+        request.get({url: baseUrl + '?t=thomas'}, (err, res, body) => {
+            let parsedBody = {};
+            try {
+                parsedBody = JSON.parse(body)
+            }
+            catch(err) {
+                parsedBody = {};
+            }
+
+            expect(res.statusCode).to.equal(200)
+
+            done();
+        })
+    })
+})
