85a64b90b9a88d5824a45ff627580767ba0fe187
Cameron Aragon <caragon4695@gmail.com>
2021-05-18 19:51:13 -0600

    Setup require variables in test file

---
+++ test/apiTest.spec.js
@@ -0,0 +1,9 @@
+const should = require('should');
+const request = require('request');
+const chai = require('chai');
+const expect = chai.expect;
+const baseUrl = 'http://www.omdbapi.com/';
+
+// describe('Initial OMDb API test', () => {
+//     it('Should return a response ')
+// })
